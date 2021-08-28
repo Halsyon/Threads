bb476d82dddc78952e0430df07022bcaf8aea5c7
SlartiBartFast-art <JekHamond@tutanota.com>
2021-08-28 14:11:06 +0500

    1.1. Состояние нити. [#229175 #195606] #rewrite

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -3,6 +3,10 @@ package ru.job4j.concurrent;
 /**
  * 1.1. Состояние нити. [#229175]
  * Уровень : 3. Мидл Категория : 3.1. Multithreading Топик : 3.1.1. Threads
+ * 1. Поправьте класс ThreadState там образом, чтобы в нем создавалось две нити.
+ * Каждая нить должна вывести свое имя на консоль.
+ * 2. Нить main должна дождаться завершения этих нитей и вывести на консоль сообщение,
+ * что работа завершена.
  */
 public class ThreadState {
     //в этой программе присутствуют две нити: main и first.
@@ -24,12 +28,11 @@ public class ThreadState {
 // что операторы описанные в конструкторе нужно запустить в отдельной нити.
         first.start();
         second.start();
-        while (first.getState() != Thread.State.TERMINATED) {
-            System.out.println("First : " + first.getState());
+        while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED) {
+            System.out.println("First : " + first.getState() + "Second : " + second.getState());
         }
-        while (second.getState() != Thread.State.TERMINATED) {
-            System.out.println("Second : " + second.getState());
-        }
-        System.out.println(Thread.currentThread().getName() + " " + first.getState());
+        System.out.println("the current thread " + Thread.currentThread().getName()
+                + "  first.getState() " + first.getState()
+                + "  second.getState() " + second.getState());
     }
 }
