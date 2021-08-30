740c524ee18d08ebfeaf9ff38ef3187372bde93f
SlartiBartFast-art <JekHamond@tutanota.com>
2021-08-30 15:04:03 +0500

    2. Модель памяти Java [#267917]

---
+++ src/main/java/ru/job4j/commonResources/DCLSingleton.java
@@ -0,0 +1,24 @@
+package ru.job4j.commonResources;
+
+/**
+ * 2. Модель памяти Java [#267917]
+ * Уровень : 3. Мидл Категория : 3.1. Multithreading Топик : 3.1.2. Общие ресурсы
+ */
+public final class DCLSingleton {
+
+    private static volatile DCLSingleton inst;
+
+    private DCLSingleton() {
+    }
+
+    public static DCLSingleton instOf() {
+        if(inst == null) {
+            synchronized (DCLSingleton.class) {
+                if (inst == null) {
+                    inst = new DCLSingleton();
+                }
+            }
+        }
+        return inst;
+    }
+}
