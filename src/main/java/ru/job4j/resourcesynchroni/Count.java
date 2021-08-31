c98718f32273afe821fa7af0442c04468094d004
SlartiBartFast-art <JekHamond@tutanota.com>
2021-08-31 20:16:55 +0500

    2. JCIP. Настройка библиотеки [#268575]

--- src/main/java/ru/job4j/resourcesynchroni/Count.java
+++ src/main/java/ru/job4j/resourcesynchroni/Count.java
@@ -1,11 +1,32 @@
 package ru.job4j.resourcesynchroni;
 
+import net.jcip.annotations.ThreadSafe;
+import net.jcip.annotations.GuardedBy;
+
 /**
  * 0. Объект монитор, критическая секция [#6857 #196743]
  * Уровень : 3. Мидл Категория : 3.1. Multithreading Топик : 3.1.3. Синхронизация ресурсов
  * Перепишем этот код для наглядности определения термина объект монитор.
+ * 2. JCIP. Настройка библиотеки [#268575]
+ * Уровень : 3. Мидл Категория : 3.1. Multithreading Топик : 3.1.3. Синхронизация ресурсов
  */
+// @ThreadSafe - говорит пользователям данного класса, что класс можно использовать
+// в многопоточном режиме и он будет работать правильно.
+// @GuardedBy("this") - выставляется над общим ресурсом. Аннотация имеет входящий параметр.
+// Он указывает на объект монитора, по которому мы будем синхронизироваться.
+@ThreadSafe
 public class Count {
+    @GuardedBy("this")
+    private int value;
+
+    public synchronized void increment() {
+        this.value++;
+    }
+
+    public synchronized int get() {
+        return this.value;
+    }
+    /*
     private int value;
 
     public void increment() {
@@ -19,4 +40,5 @@ public class Count {
             return value;
         }
     }
+    */
 }
