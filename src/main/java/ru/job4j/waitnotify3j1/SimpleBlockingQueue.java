5c283a43ba1ab8a318e85430885deaa07e29951f
SlartiBartFast-art <JekHamond@tutanota.com>
2021-09-07 20:08:01 +0500

    1. Реализовать шаблон Producer Consumer. [#1098 #198500] #02

--- src/main/java/ru/job4j/waitnotify3j1/SimpleBlockingQueue.java
+++ src/main/java/ru/job4j/waitnotify3j1/SimpleBlockingQueue.java
@@ -15,16 +15,20 @@ import java.util.Queue;
  * в очередь данные.
  * Важно !  - на мониторе какого объекта вы делает синхронизацию, на томже самом объекте
  * вы должны делать wait() и notify()
+ * wait() - освобождает монитор и переводит вызывающий поток в состояние ожидания до тех пор,
+ * пока друггой поток не вызовет метод notify();
  *
  * @param <T> generic type
  * @author SlartiBartFast-art
- * @version 0.4
+ * @version 0.5
  * @since 03.09.2021
  */
 @ThreadSafe
 public class SimpleBlockingQueue<T> {
     @GuardedBy("this")
     private Queue<T> queue = new LinkedList<>();  //блокирующая очередь ограниченная по размеру
+//поле с  типом int, которое будет ограничивать очередь сверху
+    private int count = 0;
 
     /**
      * Метод poll() должен вернуть объект из внутренней коллекции.
@@ -34,36 +38,35 @@ public class SimpleBlockingQueue<T> {
      *
      * @return Object T or null
      */
-    public T poll() { // Consumer извлекает данные из очереди
-        synchronized (this) {
-// peek()- голова этой двухсторонней очереди, или null, если эта двухсторонняя очередь пуста
-            while (queue.peek() == null) {
-                try {
-                    queue.wait(); // перевести текущую нить в состояние ожидания.
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                }
-            }
-            queue.notify(); // она отпускает объект монитор
-            return queue.poll();
+    public synchronized T poll() throws InterruptedException { // Consumer извлекает данные из очереди
+        // peek()- голова этой двухсторонней очереди, или null, если эта двухсторонняя очередь пуста
+        //while (queue.peek() == null) {
+        while (count < 1) {
+//wait() - выбрасывает InterruptedException поэтому работаем с ним в блоке try-catch
+            queue.wait(); // перевести текущую нить в состояние ожидания.
         }
+        count--;
+//notify() - не освобождает монитор и будит поток, у которого ранее был вызван метод wait();
+        queue.notify(); // она отпускает объект монитор
+        return queue.poll();
     }
 
-    public void offer(T value) { // Producer помещает данные в очередь
-        synchronized (this) {
+    public synchronized void offer(T value) throws InterruptedException { // Producer помещает данные в очередь
 //queue.offer(value) Немедленное размещение элемента в очереди при наличие свободного места;
 // метод вернет true при успешном завершении операции, в противном случае вернет false.
-            while (!queue.offer(value)) {
-                try {
+//поле count - искуственное ограничение очереди размером в 5 элементов,
+// если оно == или превышает ставим текущий поток в ожидание
+        while (count >= 5) {
 //Для того чтобы нить перевести в ждущее состояние необходимо в ее процессе вызвать метод wait()
 // для объекта монитора.
-                    queue.wait();
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                }
-            }
-            queue.offer(value);
-            queue.notify();
+            queue.wait();
         }
+        queue.offer(value);
+        count++;
+        queue.notify();
     }
 }
+
+
+
+
