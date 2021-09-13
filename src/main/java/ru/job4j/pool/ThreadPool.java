0b047c2850b1f09fa0a0009ffd54d95868a46b13
SlartiBartFast-art <JekHamond@tutanota.com>
2021-09-13 14:59:29 +0500

    1. Реализовать ThreadPool [#1099 #201968] #02

--- src/main/java/ru/job4j/pool/ThreadPool.java
+++ src/main/java/ru/job4j/pool/ThreadPool.java
@@ -1,6 +1,7 @@
 package ru.job4j.pool;
 
 import ru.job4j.waitnotify3j1.SimpleBlockingQueue;
+
 import java.util.LinkedList;
 import java.util.List;
 
@@ -14,6 +15,7 @@ import java.util.List;
  * проснуться и начать работу.
  * run() - Когда объект, реализующий интерфейс Runnable, используется для создания потока,
  * запуск потока вызывает run вызов метода объекта в этом отдельно выполняемом потоке.
+ *
  * @author SlartiBartFast-art
  * @since 11.09.2021
  */
@@ -22,8 +24,12 @@ public class ThreadPool {
     private final List<Thread> threads = new LinkedList<Thread>();
     private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>();
 
-    public ThreadPool() {
+    public ThreadPool() throws InterruptedException {
         System.out.println("SIZE :" + size);
+        for (int i = 0; i < size; i++) {
+            threads.add(i, new Thread(tasks.poll()));
+            threads.get(i).start();
+        }
     }
 
     /**
@@ -35,7 +41,6 @@ public class ThreadPool {
      */
     public synchronized void work(Runnable job) throws InterruptedException {
         tasks.offer(job);
-        notify();
     }
 
     /**
@@ -50,26 +55,12 @@ public class ThreadPool {
         }
     }
 
-    public void runoob() throws InterruptedException {
-        for (int i = 0; i <= size; i++) {
-            if (!tasks.isEmpty() && i <= 4) {
-                threads.add(i, new Thread(tasks.poll()));
-                System.out.println("INT : " + i);
-                threads.get(i).start();
-            }
-            if (tasks.isEmpty()) {
-                threads.get(i).wait();
-            }
-        }
-    }
-
     public static void main(String[] args) throws InterruptedException {
         ThreadPool threadPool = new ThreadPool();
         Job job = new Job(0);
         Job job1 = new Job(1);
         Job job2 = new Job(2);
         Job job3 = new Job(3);
-
         Job job4 = new Job(4);
 
         threadPool.work(job);
@@ -77,7 +68,6 @@ public class ThreadPool {
         threadPool.work(job2);
         threadPool.work(job3);
         threadPool.work(job4);
-        threadPool.runoob();
 
         threadPool.shutdown();
     }
