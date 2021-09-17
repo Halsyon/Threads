e135d84fbff4fa4734bc3d69d9536d40fff47392
SlartiBartFast-art <JekHamond@tutanota.com>
2021-09-17 16:00:42 +0500

    4. CompletableFuture [#361626] #03

---
+++ src/main/java/ru/job4j/pool/RolColSum.java
@@ -0,0 +1,103 @@
+package ru.job4j.pool;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 4. CompletableFuture [#361626]
+ * Уровень : 3. Мидл Категория : 3.1.
+ * MultithreadingТопик : 3.1.6. Пулы
+ * Пример использования
+ * 1. Дан каркас класса. Ваша задача подсчитать суммы по строкам и столбцам матрицы.
+ * - sums[i].rowSum - сумма элементов по i строке
+ * - sums[i].colSum  - сумма элементов по i столбцу
+ *
+ * @author SlartiBartFast-art
+ * @since 16.09.2021
+ */
+public class RolColSum {
+
+    public static class Sums {
+        private int rowSum; //rowSum - значение элемента по i строке
+        private int colSum; //rowSum - значение элемента по i столбцу
+
+        public int getRowSum() {
+            return rowSum;
+        }
+
+        public void setRowSum(int rowSum) {
+            this.rowSum = rowSum;
+        }
+
+        public int getColSum() {
+            return colSum;
+        }
+
+        public void setColSum(int colSum) {
+            this.colSum = colSum;
+        }
+
+        @Override
+        public String toString() {
+            return "Sums{"
+                    + "rowSum=" + rowSum
+                    + ", colSum=" + colSum
+                    + '}';
+        }
+    }
+
+    private static Sums[] countSum(int[][] matrix) {
+        int n = matrix.length;
+        // создаем массив по кол-ву элем-в Sums на размер матрицы
+        Sums[] sumsMatrix = new Sums[n];
+        for (int i = 0; i < matrix.length; i++) {
+            sumsMatrix[i] = new Sums();
+        }
+        int col = 0; // строка
+        int cll = 0; // строка
+        do {
+            int sum1 = 0; // сумма1
+            int sum2 = 0; // сумма2
+            for (int i = 0; i < n; i++) {
+                sum1 += matrix[col][i];
+                if (cll != n) {
+                    for (int j = 0; j < n; j++) {
+                        sum2 += matrix[j][i];
+                    }
+                    sumsMatrix[cll++].setColSum(sum2); // записали столбец 1-й, 2, 3
+                    sum2 = 0;
+                }
+            }
+            sumsMatrix[col].setRowSum(sum1); //записал сумму 1-й строки, 2, 3
+            col++;
+        } while (col != n);
+        return sumsMatrix; // вернули массив из Object Sums
+    }
+
+    //sums[i].rowSum - сумма элементов по i строке
+    //Эти методы должны посчитать одно и тоже - сумму строки и столбца в массиве
+    public static Sums[] sum(int[][] matrix) {
+        return countSum(matrix);
+    }
+
+    public static Sums[] asyncSum(int[][] matrix) throws ExecutionException, InterruptedException {
+        int n = matrix.length;
+        Sums[] sumsMatrix = new Sums[n];
+        Map<Integer, CompletableFuture<Sums[]>> futures = new HashMap<>();
+        futures.put(0, getTask(matrix));
+        for (Integer key : futures.keySet()) {
+            var t = futures.get(key).get();
+            for (int i = 0; i < t.length; i++) {
+                sumsMatrix[i] = t[i];
+            }
+        }
+        return sumsMatrix;
+    }
+
+    public static CompletableFuture<Sums[]> getTask(int[][] data) {
+        return CompletableFuture.supplyAsync(() -> countSum(data));
+    }
+}
---
+++ src/test/java/ru/job4j/pool/RolColSumTest.java
@@ -0,0 +1,62 @@
+package ru.job4j.pool;
+
+import org.hamcrest.core.Is;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.concurrent.ExecutionException;
+
+import static org.junit.Assert.*;
+
+/**
+ * 4. CompletableFuture [#361626]
+ * Уровень : 3. Мидл Категория : 3.1.
+ * MultithreadingТопик : 3.1.6. Пулы
+ *
+ * @author SlartiBartFast-art
+ * @since 17.09.2021
+ */
+public class RolColSumTest {
+    private int[][] str;
+    private int[][] str1;
+
+    @Before
+    public void init() {
+        str = new int[3][3];
+        str[0][0] = 1;
+        str[0][1] = 2;
+        str[0][2] = 3;
+        str[1][0] = 4;
+        str[1][1] = 5;
+        str[1][2] = 6;
+        str[2][0] = 7;
+        str[2][1] = 8;
+        str[2][2] = 9;
+        str1 = new int[2][3];
+        str1[0][0] = 1;
+        str1[0][1] = 2;
+        str1[0][2] = 3;
+        str1[1][0] = 4;
+        str1[1][1] = 5;
+        str1[1][2] = 5;
+    }
+
+    @Test
+    public void whenAsyncSum1() throws ExecutionException, InterruptedException {
+        String d = "[Sums{rowSum=6, colSum=12}, Sums{rowSum=15, colSum=15}, Sums{rowSum=24, colSum=18}]";
+        assertThat(Arrays.toString(RolColSum.asyncSum(str)), Is.is(d));
+    }
+
+    @Test
+    public void whenAsyncSum2() throws ExecutionException, InterruptedException {
+        String d = "[Sums{rowSum=3, colSum=5}, Sums{rowSum=9, colSum=7}]";
+        assertThat(Arrays.toString(RolColSum.asyncSum(str1)), Is.is(d));
+    }
+
+    @Test
+    public void whenSum() {
+        String d = "[Sums{rowSum=6, colSum=12}, Sums{rowSum=15, colSum=15}, Sums{rowSum=24, colSum=18}]";
+        assertThat(Arrays.toString(RolColSum.sum(str)), Is.is(d));
+    }
+}
