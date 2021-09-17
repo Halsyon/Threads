756750c4b8fd14082eccf110383e0cd150c401c1
SlartiBartFast-art <JekHamond@tutanota.com>
2021-09-17 13:35:14 +0500

    3. ForkJoinPool [#315067 #203185] #04

--- src/test/java/ru/job4j/pool/ParallelMergeSortArrTest.java
+++ src/test/java/ru/job4j/pool/ParallelMergeSortArrTest.java
@@ -17,35 +17,6 @@ import static org.junit.Assert.*;
  * @since 14.09.2021
  */
 public class ParallelMergeSortArrTest {
-    @Test
-    public void whenAddThenGet() {
-        Integer[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-        ParallelMergeSortArr mergeSortArr = new ParallelMergeSortArr(arr, 0, arr.length, 3);
-        Integer arr1 = 2;
-        var f = mergeSortArr.compute();
-        assertThat(f, Is.is(arr1));
-    }
-
-    @Test
-    public void whenAdd9() {
-        Integer[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        ParallelMergeSortArr mergeSortArr = new ParallelMergeSortArr(arr, 0, arr.length, 5);
-        var f = mergeSortArr.compute();
-        Integer arr1 = 4;
-        assertThat(f, Is.is(arr1));
-    }
-
-    @Test
-    public void whenAdd30() {
-        Integer[] arr = new Integer[30];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = i;
-        }
-        ParallelMergeSortArr mergeSortArr = new ParallelMergeSortArr(arr, 0, arr.length, 11);
-        Integer arr1 = 11;
-        var f = mergeSortArr.compute();
-        assertThat(f, Is.is(arr1));
-    }
 
     @Test
     public void when1() {
