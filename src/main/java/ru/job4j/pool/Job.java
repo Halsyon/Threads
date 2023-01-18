2bbf4d46f5a3d737b3251238867df14bd52c849a
Halsyon <73285357+Halsyon@users.noreply.github.com>
2023-01-18 23:50:51 +0500

    Update Job.java

--- src/main/java/ru/job4j/pool/Job.java
+++ src/main/java/ru/job4j/pool/Job.java
@@ -2,9 +2,6 @@ package ru.job4j.pool;
 
 /**
  * 1. Реализовать ThreadPool [#1099]
- * Уровень : 3. Мидл Категория : 3.1.
- * Multithreading Топик : 3.1.6. Пулы
- * @author SlartiBartFast-art
  * @since 11.09.2021
  */
 public class Job implements Runnable {
