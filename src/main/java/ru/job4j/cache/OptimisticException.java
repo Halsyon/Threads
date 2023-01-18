56231b8638051f0a2ea46630708515618aa50ee8
Halsyon <73285357+Halsyon@users.noreply.github.com>
2023-01-18 23:52:27 +0500

    Update OptimisticException.java

--- src/main/java/ru/job4j/cache/OptimisticException.java
+++ src/main/java/ru/job4j/cache/OptimisticException.java
@@ -2,7 +2,6 @@ package ru.job4j.cache;
 
 /**
  * 1. Неблокирующий кеш [#4741]
- * Уровень : 3. Мидл Категория : 3.1. Multithreading
  * Топик : 3.1.5. Non Blocking Algorithm
  * В кеше же нужно перед обновлением данных проверить поле version.
  * Если version у модели и в кеше одинаковы, то можно обновить.
@@ -11,7 +10,6 @@ package ru.job4j.cache;
  * и увеличивать на единицу каждый раз, когда произошло обновление.
  * Если версии не равны -  кидать исключение.
  * часть 3
- * @author SlartiBartFast-art
  * @since 09.09.2021
  */
 public class OptimisticException extends RuntimeException {
