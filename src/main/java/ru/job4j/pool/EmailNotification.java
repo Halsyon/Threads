b5248f52fd67ec22bedcf7893cf6a979b01a2bd9
Halsyon <73285357+Halsyon@users.noreply.github.com>
2023-01-19 00:18:14 +0500

    Update EmailNotification.java

--- src/main/java/ru/job4j/pool/EmailNotification.java
+++ src/main/java/ru/job4j/pool/EmailNotification.java
@@ -4,23 +4,9 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 /**
- * 2. ExecutorService рассылка почты. [#63097]
- * Уровень : 3. Мидл Категория : 3.1. Multithreading
+ * 2. ExecutorService рассылка почты. 
  * Топик : 3.1.6. Пулы
  * часть 2
- * Реализовать сервис для рассылки почты. Создайте класс EmailNotification.
- * пул = Executors.newFixedThreadPool (размер пула);
- * В классе будет метод emailTo(User user) -
- * Так же добавьте метод close() - он должен закрыть pool.
- * То есть в классе EmailNotification должно быть поле pool,
- * которые используется в emailTo и close().
- * В классе будет метод emailTo(User user) -
- * он должен через ExecutorService отправлять почту.
- * Метод emailTo должен брать данные пользователя и подставлять в шаблон
- * subject = Notification {username} to email {email}.
- * body = Add a new event to {username}
- *
- * @author SlartiBartFast-art
  * @since 13.09.2021
  */
 public class EmailNotification {
